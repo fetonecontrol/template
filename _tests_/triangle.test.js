eaf1fb9d35f7bc3935eeaf6eae005e388e9f8403
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-23 09:44:22 -0700

    create working template boilerplate

---
+++ _tests_/triangle.test.js
@@ -0,0 +1,9 @@
+import { Triangle } from './../src/triangle.js';
+
+describe('Triangle', () => {
+
+  test('should correctly determine whether three lengths make an isosceles triangle', () => {
+    const isocTriangle = new Triangle(5,5,7)
+    expect(isocTriangle.checkType()).toEqual("isosceles triangle");
+  });
+});
