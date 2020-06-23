d38ba5854a7836892b06d8bb2ed1e463b24b3e59
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-23 09:56:10 -0700

    remove erronious code

--- _tests_/triangle.test.js
+++
@@ -1,9 +0,0 @@
-import { Triangle } from './../src/triangle.js';
-
-describe('Triangle', () => {
-
-  test('should correctly determine whether three lengths make an isosceles triangle', () => {
-    const isocTriangle = new Triangle(5,5,7)
-    expect(isocTriangle.checkType()).toEqual("isosceles triangle");
-  });
-});
